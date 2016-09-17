396638d29b096ef3b501df72522fe5e0d24de055
jsbean <jbean46@gmail.com>
2016-09-17 10:52:36 -0400

    Normalize whitespace

--- ControllerElements/Dial.swift
+++ ControllerElements/Dial.swift
@@ -63,7 +63,6 @@ public class Dial: CALayer, CompositeShapeType {
         createComponents()
     }
     
-    
     public required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
