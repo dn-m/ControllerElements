22e1a67c7c38ee6ece1650f709b8a2c7fb363b75
jsbean <jbean46@gmail.com>
2016-09-19 11:46:27 -0400

    Create ContinuousController protocol with ramp(to:over:)

---
+++ ControllerElements/ContinuousController.swift
@@ -0,0 +1,12 @@
+//
+//  ContinuousController.swift
+//  ControllerElements
+//
+//  Created by James Bean on 9/19/16.
+//
+//
+
+public protocol ContinuousController {
+    
+    func ramp(to value: Float, over duration: Double)
+}
